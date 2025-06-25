39d44c3577ce784ad98ae317f83fa133ea563f2d
Park Jongwoon <nortiz1349@gmail.com>
2025-06-25 15:01:25 +0900

    Package clean up

--- Package.swift
+++ Package.swift
@@ -1,7 +1,31 @@
 // swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
+
+let isLocal = Context.environment["VALHALLA_DEV"].flatMap(Bool.init) ?? false
+
+var cxxSettings: [CXXSetting] {
+    if isLocal {
+        [.headerSearchPath("build/valhalla-wrapper.xcframework/macos-arm64/Headers/include")]
+    } else {
+        []
+    }
+}
+
+var binaryTarget: Target {
+    if isLocal {
+        .binaryTarget(
+            name: "ValhallaWrapper",
+            path: "build/valhalla-wrapper.xcframework"
+        )
+    } else {
+        .binaryTarget(
+            name: "ValhallaWrapper",
+            url: "https://github.com/nortiz1349/valhalla-vapor/releases/download/0.1.0/valhalla-wrapper.xcframework.zip",
+            checksum: "fa924c7256f4522f1aaabf192c27ca4656be9636404c5ca756805ad5f5812148"
+        )
+    }
+}
 
 let package = Package(
     name: "valhalla-vapor",
@@ -21,7 +45,6 @@ let package = Package(
         .target(
             name: "Valhalla",
             dependencies: [
-//                "ValhallaObjc",
                 "ValhallaWrapper",
                 .product(name: "ValhallaConfigModels", package: "valhalla-openapi-models-swift"),
                 .product(name: "ValhallaModels", package: "valhalla-openapi-models-swift"),
@@ -31,28 +54,12 @@ let package = Package(
             resources: [
                 .process("SupportData")
             ],
-//            cxxSettings: [
-//                .headerSearchPath("build/valhalla-wrapper.xcframework/macos-arm64/Headers/include"),
-//            ],
+            cxxSettings: cxxSettings,
             swiftSettings: [
                 .interoperabilityMode(.Cxx),
             ],
         ),
-//        .target(
-//            name: "ValhallaObjc",
-//            dependencies: ["ValhallaWrapper"],
-//            path: "Sources/ValhallaObjc",
-//            linkerSettings: [.linkedLibrary("z")]
-//        ),
-//        .binaryTarget(
-//            name: "ValhallaWrapper",
-//            path: "build/valhalla-wrapper.xcframework"
-//        ),
-        .binaryTarget(
-            name: "ValhallaWrapper",
-            url: "https://github.com/nortiz1349/valhalla-vapor/releases/download/0.1.0/valhalla-wrapper.xcframework.zip",
-            checksum: "fa924c7256f4522f1aaabf192c27ca4656be9636404c5ca756805ad5f5812148"
-        )
+        binaryTarget
     ],
     cLanguageStandard: .gnu17,
     cxxLanguageStandard: .cxx17
